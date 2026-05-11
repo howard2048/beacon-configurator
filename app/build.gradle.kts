a351ad3af538686bb523822b15ec8e0f30a967b1
Howard <wanghao.sh.cn@gmail.com>
2026-05-12 01:21:05 +0800

    bugfix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.compose.material.icons.core)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
@@ -51,6 +52,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.compose.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/cn/landingsite/bc/BeaconListPage.kt
+++ app/src/main/java/cn/landingsite/bc/BeaconListPage.kt
@@ -12,14 +12,27 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -29,14 +42,27 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BeaconListPage(
     onItemGoConfig: () -> Unit
 ) {
     val activity = LocalContext.current as MainActivity
     val beacons = remember { mutableStateListOf<Beacon>() }
-    val scanning = remember { mutableStateOf(false) }
+    var isScanning by remember { mutableStateOf(false) }
+    var isRefreshing by remember { mutableStateOf(false) }
+    val refreshState = rememberPullToRefreshState()
+    val scope = rememberCoroutineScope()
+
+    var scanCountdownJob by remember { mutableStateOf<Job?>(null) }
+    val scanDuration = 5000L
+
+    val colorScheme = MaterialTheme.colorScheme
 
     val isGattReady by BleConnectManager.gattReady.collectAsStateWithLifecycle()
     if (isGattReady) {
@@ -46,60 +72,108 @@ fun BeaconListPage(
     val handleConnectCLick = {
 
     }
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
+    MaterialTheme(
+        colorScheme = darkColorScheme()
     ) {
-        Button(
-            onClick = {
-                if (!scanning.value) {
-                    beacons.clear()
-                    activity.startBleScan { beacon ->
-                        // 先找列表里有没有同MAC设备
-                        val pos = beacons.indexOfFirst { it.mac == beacon.mac }
-                        if (pos == -1) {
-                            // 没有：新增
-                            beacons.add(beacon)
-                        } else {
-                            // 已有：直接覆盖更新最新RSSI和信息
-                            beacons[pos] = beacon
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(
+                            text = "Beacon Configurator",
+                        )
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = {}) {
+                            Icon(Icons.Filled.Menu, contentDescription = "Menu")
                         }
+                    },
+                )
+            }
+        ) { innerPadding ->
+            PullToRefreshBox(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+                isRefreshing = isRefreshing,
+                onRefresh = {
+                    scope.launch {
+                        Log.i(TAG, "refreshing...")
+                        if (isScanning) {
+                            scanCountdownJob?.cancel()
+                        } else {
+                            isScanning = true
+                            Log.i(TAG, "scan for ${scanDuration / 1000} seconds ")
+                            beacons.clear()
+                            activity.startBleScan { beacon ->
+                                val pos = beacons.indexOfFirst { it.mac == beacon.mac }
+                                if (pos == -1) {
+                                    beacons.add(beacon)
+                                } else {
+                                    beacons[pos] = beacon
+                                }
 //                        beacons.sortByDescending { it.rssi }
+                            }
+                        }
+                        scanCountdownJob = scope.launch {
+                            delay(scanDuration)
+                            Log.i(TAG, "stop scan")
+                            activity.stopBleScan()
+                            isScanning = false
+                        }
+                        isRefreshing = false
+                        refreshState.animateToHidden()
                     }
-                    scanning.value = true
-                } else {
-//                    beacons.clear()
-                    activity.stopBleScan()
-                    scanning.value = false
-                }
-            }, modifier = Modifier.fillMaxWidth()
-        ) {
-            Text(if (scanning.value) "停止扫描" else "开始扫描 Beacon")
-        }
 
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(top = 16.dp)
-        ) {
-            items(beacons) { item ->
-                BeaconListItem(beacon = item, onConnectClick = { beacon ->
-                    if (ActivityCompat.checkSelfPermission(
-                            activity,
-                            Manifest.permission.BLUETOOTH_CONNECT
-                        ) != PackageManager.PERMISSION_GRANTED
-                    ) {
-                        return@BeaconListItem
-                    }
+
+//            if (!isScanning.value) {
+//                beacons.clear()
+//                activity.startBleScan { beacon ->
+//                    // 先找列表里有没有同MAC设备
+//                    val pos = beacons.indexOfFirst { it.mac == beacon.mac }
+//                    if (pos == -1) {
+//                        // 没有：新增
+//                        beacons.add(beacon)
+//                    } else {
+//                        // 已有：直接覆盖更新最新RSSI和信息
+//                        beacons[pos] = beacon
+//                    }
+////                        beacons.sortByDescending { it.rssi }
+//                }
+//                isScanning.value = true
+//            } else {
+////                    beacons.clear()
+//                activity.stopBleScan()
+//                isScanning.value = false
+//            }
+                },
+                state = refreshState
+            ) {
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    items(beacons) { item ->
+                        BeaconListItem(beacon = item, onConnectClick = { beacon ->
+                            if (ActivityCompat.checkSelfPermission(
+                                    activity,
+                                    Manifest.permission.BLUETOOTH_CONNECT
+                                ) != PackageManager.PERMISSION_GRANTED
+                            ) {
+                                return@BeaconListItem
+                            }
 //                    val result = BleConnectManager.connectDevice(activity, beacon.mac)
-                    handleConnectCLick()
-                    onItemGoConfig()
-                })
+                            handleConnectCLick()
+                            onItemGoConfig()
+                        })
+                    }
+                }
+
             }
         }
     }
+
+
 }
 
 const val TAG = "X"
